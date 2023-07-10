67b1d27a74beddc1b9a4626780da3058ef1c8ec1
I-bug <omaugulu@gmail.com>
2023-07-10 18:14:51 +0100

    Area of a rectangle task 9

---
+++ Module 3/Module3A/task9.js
@@ -0,0 +1,6 @@
+function calculateAreaOfRectangle(length, width) { 
+    const area = length * width;
+    
+    console.log("Area of the Rectangle is: " + area)
+}
+calculateAreaOfRectangle(10.5, 2.5);

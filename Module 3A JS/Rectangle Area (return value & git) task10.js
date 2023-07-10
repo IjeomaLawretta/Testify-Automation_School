fa33ec57237574df0fb259597de769d0d467a5b1
I-bug <omaugulu@gmail.com>
2023-07-10 18:19:27 +0100

    Friends array task

--- Module 3A JS/Rectangle Area (return value & git) task10.js
+++
@@ -1,8 +0,0 @@
-
-function calculateAreaOfRectangle(length, width) {
-    const area = length * width;
-    return area;
-}
-const moreOp = calculateAreaOfRectangle(10.5, 2.5)
-
-console.log(moreOp);

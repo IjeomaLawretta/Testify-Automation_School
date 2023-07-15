16fa8eb614ba889a845d96f4de3dd12b648c0405
I-bug <omaugulu@gmail.com>
2023-07-15 10:02:01 +0100

    Module 3A modifications

--- Module3/Module3A_JavaScriptCore/task7_types_of_triangle.js
+++ Module3/Module3A_JavaScriptCore/task7_types_of_triangle.js
@@ -6,7 +6,7 @@ const side2= 17
 const side3= 10
 
 if(side1===side2 && side2===side3){
-    console.log('Equilateral trainagle')
+    console.log('Equilateral triangle')
 }else if(side1===2 || side2===side3 || side1===side3){
     console.log('Isoceles triangle')
 }else {
